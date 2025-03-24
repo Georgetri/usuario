13bfece7cdd971a60b6517807cde537cd2bec97a
Jorge <georgetri@icloud.com>
2025-03-24 16:25:52 -0400

    corrigindo imports quebrados

--- src/main/java/com/gestaousuario/usuario/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/gestaousuario/usuario/infrastructure/repository/EnderecoRepository.java
@@ -1,6 +1,6 @@
 package com.gestaousuario.usuario.infrastructure.repository;
 
-import com.george.springbasic.infrastructure.entity.Endereco;
+import com.gestaousuario.usuario.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
