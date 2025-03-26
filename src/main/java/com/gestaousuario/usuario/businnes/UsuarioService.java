6d08521585f0de85f387829f4f266eae2d0ae3ee
Jorge <georgetri@icloud.com>
2025-03-26 15:20:35 -0400

    correções na classe UsuarioService

--- src/main/java/com/gestaousuario/usuario/businnes/UsuarioService.java
+++ src/main/java/com/gestaousuario/usuario/businnes/UsuarioService.java
@@ -3,9 +3,11 @@ package com.gestaousuario.usuario.businnes;
 import com.gestaousuario.usuario.businnes.converter.UsuarioConverter;
 import com.gestaousuario.usuario.businnes.dto.UsuarioDTO;
 import com.gestaousuario.usuario.infrastructure.entity.Usuario;
+import com.gestaousuario.usuario.infrastructure.exceptions.ConflitException;
 import com.gestaousuario.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.gestaousuario.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,12 +16,31 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
+        emailExiste(usuarioDTO.getEmail());
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
         Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
-        usuario = usuarioRepository.save(usuario);
-        return usuarioConverter.paraUsuarioDTO(usuario);
-     // return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+    }
+
+
+    public void emailExiste(String email){
+        try {
+            boolean existe = verificaEmailExistente(email);
+            if(existe){
+                throw new ConflitException("Email já cadastrado "+email);
+            }
+
+        } catch (ConflitException e) {
+            throw new ConflitException("E-mail já cadastrado "+e.getCause());
+        }
+
+    }
+
+    public boolean verificaEmailExistente(String email){
+        return usuarioRepository.existsByEmail(email);
     }
 
 
